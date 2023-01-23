ae7e49cf73c38186e7f23112c152680dfcdd9fba
Artem Nilov <nilov1235@mail.ru>
2023-01-23 14:42:12 +0300

    Add project files.

---
+++ CshEzam2Quiz/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CshEzam2Quiz
+{
+    internal class User
+    {
+        private string _login;
+        private string _password;
+        private DateTime _birthDay;
+
+        public User(string login, string password, DateTime birthDay)
+        {
+            _login = login;
+            _password = password;
+            _birthDay = birthDay;
+        }
+
+        public void ChangePassword(string newPass)
+        {
+            _password = newPass;
+        }
+
+        public void ChangeBirthDay(DateTime newBirthDAy)
+        {
+            _birthDay=newBirthDAy;
+        }
+
+        // находит логин в файле и меняет его пароль и день рождения
+        public void SaveToFile(string login)
+        {
+
+        }
+    }
+}
